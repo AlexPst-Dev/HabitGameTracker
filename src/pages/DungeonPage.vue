2666bbdc5c9c868e2ab15bbb47031db28ba27084
AlexPst-Dev <alexandre-proust.dev@proton.me>
2025-01-20 23:43:10 +0100

    feat: add Dungeon page

---
+++ src/pages/DungeonPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <main>
+    <h1>Dungeon</h1>
+  </main>
+</template>
+
+<script setup>
+import "@/assets/style.css";
+</script>
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
+import DungeonPage from "./pages/DungeonPage.vue";
 import HomePage from "./pages/HomePage.vue";
 
 export const routes = [
   {
     path: "/",
     component: HomePage,
+  },
+  {
+    path: "/game",
+    component: DungeonPage,
   },
 ];
